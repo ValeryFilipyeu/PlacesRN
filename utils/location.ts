22a5883a9a41128dc1ad5026d9a28eecb2b87f08
Valery Filipyev <valeranuzhdin1998@mail.ru>
2023-05-03 00:31:56 +0300

    slightly change utils and tests

--- utils/location.ts
+++ utils/location.ts
@@ -1,13 +1,11 @@
 import { GEOCODING_BASE_URL } from "../constants/urls";
 
 type MapPreviewOptions = {
-  width: number;
-  height: number;
   zoom?: number;
   marker?: boolean;
 };
 
-export async function getAddress(lat: number, lng: number) {
+export async function getAddress(lat: number, lng: number): Promise<string> {
   const url = `${GEOCODING_BASE_URL}/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
   const response = await fetch(url);
 
@@ -20,22 +18,17 @@ export async function getAddress(lat: number, lng: number) {
   return data.display_name as string;
 }
 
-export async function getMapPreview(lat: number, lng: number, options: MapPreviewOptions): Promise<string> {
-  const { width, height, zoom = 15, marker = true } = options;
-  const markerParams = marker ? `marker=${lat},${lng}` : '';
-  const url = `https://www.openstreetmap.org/export/embed.html?bbox=${lng - 0.01},${lat - 0.01},${lng + 0.01},${lat + 0.01}&layer=mapnik&marker=${lat},${lng}&zoom=${zoom}&${markerParams}`;
+export function getMapPreview(
+  lat: number,
+  lng: number,
+  options: MapPreviewOptions
+): string {
+  const { zoom = 15, marker = true } = options;
+  const markerParams = marker ? `marker=${lat},${lng}` : "";
 
-  const response = await fetch(url);
-  const html = await response.text();
-
-  const regex = /src="(.*?)"/;
-  const match = regex.exec(html);
-
-  if (match && match.length > 1) {
-    const imageUrl = match[1];
-
-    return `${imageUrl}&width=${width}&height=${height}`;
-  } else {
-    throw new Error('Failed to retrieve map preview image URL');
-  }
+  return `https://www.openstreetmap.org/export/embed.html?bbox=${lng - 0.01},${
+    lat - 0.01
+  },${lng + 0.01},${
+    lat + 0.01
+  }&layer=mapnik&marker=${lat},${lng}&zoom=${zoom}&${markerParams}`;
 }
